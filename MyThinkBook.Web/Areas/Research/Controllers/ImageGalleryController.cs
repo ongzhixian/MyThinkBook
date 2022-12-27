1996b8b48ba28862e32b94f783dfa1c8a2e7d90d
zhixian <zhixian@hotmail.com>
2022-12-27 23:31:10 +0800

    update

---
+++ MyThinkBook.Web/Areas/Research/Controllers/ImageGalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyThinkBook.Web.Areas.Research.Controllers;
+
+[Area("Research")]
+public class ImageGalleryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
