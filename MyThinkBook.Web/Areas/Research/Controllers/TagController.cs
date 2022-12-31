95e137567224c1555a778341f19f3dd3bc87ae39
zhixian <zhixian@hotmail.com>
2022-12-31 10:02:08 +0800

    update

---
+++ MyThinkBook.Web/Areas/Research/Controllers/TagController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyThinkBook.Web.Areas.Research.Controllers;
+
+[Area("Research")]
+public class TagController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Tag()
+    {
+        return View();
+    }
+
+    public IActionResult Relation()
+    {
+        return View();
+    }
+}
